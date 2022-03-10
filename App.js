04ab700f7ceb2036187fb254ab2bb74a37dc307f
Alun Davey <hey@4lun.io>
2022-03-10 12:15:54 +0000

    Base Expo project with @portabletext/react

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PortableText } from '@portabletext/react';
+
+const content = [
+  {
+    _type: "block",
+    _key: "head",
+    style: "h1",
+    markDefs: [],
+    children: [{ _type: "span", text: "@portabletext/react" }]
+  }
+];
+
+const components = {
+  block: {
+    h1: ({ children }) => (<Text style={styles.h1}>{children}</Text>)
+  }
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <PortableText
+				value={content}
+        components={components}
+				listNestingMode="direct"
+			/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +38,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  h1: {
+    fontSize: 32,
+  }
 });
